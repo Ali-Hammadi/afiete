1e1f4a9803a1510a73a6808b7de404380c7eb8e4
Ali-Hammadi <hamadea524@gmail.com>
2026-04-05 13:27:47 +0300

    create doctor entity in doctor feature

---
+++ lib/feature/doctors/domain/entites/doctor_entity.dart
@@ -0,0 +1,17 @@
+class DoctorEntity {
+  final String name;
+  final String specialization;
+  final String experience;
+  final String rating;
+  final String imageUrl;
+  final String description;
+
+  DoctorEntity({
+    required this.name,
+    required this.specialization,
+    required this.experience,
+    required this.rating,
+    required this.imageUrl,
+    required this.description,
+  });
+}
